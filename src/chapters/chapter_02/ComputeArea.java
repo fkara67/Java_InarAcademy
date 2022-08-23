cd17ea7c2a287c98f993d344d5cdea54a88078bc
Fatih Kara <fkara1667@gmail.com>
2022-08-23 23:46:47 +0300

    New Additions

---
+++ src/chapters/chapter_02/ComputeArea.java
@@ -0,0 +1,17 @@
+package chapters.chapter_02;
+
+public class ComputeArea {
+	 public static void main(String[] args) {
+		 double radius;
+		 double area;
+		 radius=20;
+		 area=radius * radius * 3.14159;
+		 System.out.println("The area for the circle of radius " +
+		 radius + " is" +  area);
+	 }
+		
+	}
+		 
+	
+
+
---
+++ src/weeks/week_02/Exercise2_1.java
@@ -0,0 +1,6 @@
+package weeks.week_02;
+
+public class Exercise2_1 {
+	
+
+}
