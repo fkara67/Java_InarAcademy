d1895c133f139cc5b09522b88494262b3b0d80c7
Fatih Kara <fkara1667@gmail.com>
2022-08-24 12:18:37 +0300

    new additions

---
+++ src/chapters/chapter_02/ComputeAreaWithConstant.java
@@ -0,0 +1,18 @@
+package chapters.chapter_02;
+
+import java.util.Scanner;
+
+public class ComputeAreaWithConstant {
+	public static void main(String[] args) {
+		final double PI = 3.14159;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a number for radius: ");
+		double radius = input.nextDouble();
+		double area = radius * radius * PI;
+		System.out.println("The area of circle for radius " + radius + " is " + area);
+		
+	}
+	
+	
+
+}
---
+++ src/chapters/chapter_02/FindError.java
@@ -0,0 +1,10 @@
+package chapters.chapter_02;
+
+public class FindError {
+		 public static void main(String[] args) {
+			//int j,k;
+			int i = j = k = 2; //j and k must be identified before this step like one step above
+			System.out.println(i + "" + j + "" + k);
+		 }
+
+}
