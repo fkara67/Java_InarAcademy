77cf7cffd6abcf7afa75e31729936cfa80e2e342
Fatih Kara <fkara1667@gmail.com>
2022-08-25 23:25:09 +0300

    thisJobWillBe

--- src/chapters/chapter_02/Exercises2_9.java
+++ src/chapters/chapter_02/Exercises2_9.java
@@ -1,4 +1,5 @@
 package chapters.chapter_02;
+
 import java.util.Scanner;
 
 public class Exercises2_9 {
@@ -10,11 +11,7 @@ public class Exercises2_9 {
 		double v1 = input.nextDouble();
 		double t = input.nextDouble();
 		double averageAcceleration = (v1 - v0) / t;
-		averageAcceleration = (averageAcceleration / 100) * 100;
+		averageAcceleration = (int)(averageAcceleration * 100) / 100.0;
 		System.out.println("The average acceleration is: " + averageAcceleration);
-		
-		
-
 	}
-
 }
