b23dda5bf3b29964bc75a4941f68fe2881c9bb89
Fatih Kara <fkara1667@gmail.com>
2022-08-25 20:39:03 +0300

    new

--- src/chapters/chapter_02/Exercises2_8.java
+++ src/chapters/chapter_02/Exercises2_8.java
@@ -1,9 +1,23 @@
 package chapters.chapter_02;
-
+import java.util.Scanner;
 public class Exercises2_8 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	Scanner input= new Scanner(System.in);
+	System.out.println("Enter the time zone offset to GMT: ");
+	long zoneOffset = input.nextLong();
+	long totalMilliseconds = System.currentTimeMillis();
+	long totalSeconds = totalMilliseconds / 1000;
+	long currentSecond = totalSeconds % 60;
+	long totalMinutes = totalSeconds / 60;
+	long currentMinute = totalMinutes % 60;
+	long totalHour = totalMinutes / 60;
+	long currentHour = totalHour % 24;
+	currentHour = currentHour + zoneOffset;
+	System.out.println("The current time is : " + currentHour + ":" + currentMinute + ":" + currentSecond + " GMT" + zoneOffset );
+	
+	
+		
 
 	}
 
---
+++ src/chapters/chapter_02/Exercises2_9.java
@@ -0,0 +1,20 @@
+package chapters.chapter_02;
+import java.util.Scanner;
+
+public class Exercises2_9 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter v0, v1 and t: ");
+		double v0 = input.nextDouble();
+		double v1 = input.nextDouble();
+		double t = input.nextDouble();
+		double averageAcceleration = (v1 - v0) / t;
+		averageAcceleration = (averageAcceleration / 100) * 100;
+		System.out.println("The average acceleration is: " + averageAcceleration);
+		
+		
+
+	}
+
+}
