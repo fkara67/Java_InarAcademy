4f231689b069107426f164a61018ec47146b2fe0
Fatih Kara <fkara1667@gmail.com>
2022-08-26 18:36:40 +0300

    itIsFinishedButMeToo

---
+++ src/chapters/chapter_02/Exercises2_23.java
@@ -0,0 +1,21 @@
+package chapters.chapter_02;
+import java.util.Scanner;
+public class Exercises2_23 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the driving distance: ");
+		System.out.println("Enter the miles per gallon: ");
+		System.out.println("Enter the prices per gallon: ");
+		double distance = input.nextDouble();
+		double milesPerGallon = input.nextDouble();
+		double pricePerGallon = input.nextDouble();
+		double costOfDriving = distance / milesPerGallon * pricePerGallon;
+		costOfDriving = (int)(costOfDriving * 100) / 100.0;
+		System.out.println("The cost of driving is: " + costOfDriving);
+		
+		
+		
+	}
+
+}
