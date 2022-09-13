97b1903de4a1ed464a40e2baf7e66ccb516a3afa
Fatih Kara <fkara1667@gmail.com>
2022-09-13 23:58:12 +0300

    new

--- src/chapters/chapter_05/Exercises05_17.java
+++ src/chapters/chapter_05/Exercises05_17.java
@@ -6,13 +6,27 @@ public class Exercises05_17 {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter an integer from 0 to 15: ");
+		System.out.print("Enter an integer from 1 to 15: ");
 		int number = input.nextInt();
-		
-		for (int count = 1; count < number + 1; count++) {
-			System.out.print(count);
-			System.out.println(count + (count - 1) + count);
-		}
+
+		if (number >= 1 && number <= 15) {
+			for (int row = 1; row <= number; row++) {
+				for (int whiteSpace = (number - row); whiteSpace > 0; whiteSpace--) {
+					System.out.print("   ");
+				}
+				for (int part2 = row; part2 >= 1; part2--) {
+					System.out.print("  " + part2);
+
+				}
+				for (int part3 = 2; part3 <= row; part3++) {
+					System.out.print("  " + part3 + "");
+				}
+				System.out.println();
+			}
+			
+
+		} else
+			System.out.println("ıt is an invalid input!");
 	}
 
 }
--- src/chapters/chapter_05/Exercises05_18.java
+++ src/chapters/chapter_05/Exercises05_18.java
@@ -3,7 +3,17 @@ package chapters.chapter_05;
 public class Exercises05_18 {
 
 	public static void main(String[] args) {
-		
+		for (int row = 1; row <= 6; row++) {
+
+			for (row = 1; row <= 6; row++) {
+				System.out.print(" " + row);
+
+			}
+			for (int white = 1; white > 6; white++) {
+				System.out.print("------ ");
+			}
+			System.out.println();
+		}
 	}
 
 }
